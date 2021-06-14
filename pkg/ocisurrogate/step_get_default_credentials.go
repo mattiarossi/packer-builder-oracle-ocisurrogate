94675f68d2d73d217cb49969c1ca579f194ea316
Mathew Crane <mathewcrane@fico.com>
2021-06-14 14:09:02 -0400

    Fix indent discrepancy

--- pkg/ocisurrogate/step_get_default_credentials.go
+++ pkg/ocisurrogate/step_get_default_credentials.go
@@ -51,7 +51,7 @@ func (s *stepGetDefaultCredentials) Run(ctx context.Context, state multistep.Sta
 	}
 
 	// store so that we can access this later during provisioning
-        state.Put("winrm_password", s.Comm.WinRMPassword)
+	state.Put("winrm_password", s.Comm.WinRMPassword)
 	packer.LogSecretFilter.Set(s.Comm.WinRMPassword)
 	return multistep.ActionContinue
 }
